29d624736f2d64be38a9a0945c6eb15e85327a53
KorablevDO <krablevd@ya.ru>
2018-02-10 14:54:46 +0300

    Add

---
+++ src/main/java/Project/Test.java
@@ -0,0 +1,76 @@
+package Project;
+
+/**
+ * 1. Реализовать много поточность.
+ * 2. Переключение между потоками.
+ * 3.
+ *
+ * Полезно:
+ * 1. https://habrahabr.ru/post/164487/
+ * 2.
+ */
+public class Test {
+    private int lenght = 100;
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.lenght);
+
+        Thread thread1 = new Thread(new Runnable() {
+            public void run() {
+                Test test = new Test();
+                test.printX();
+            }
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            public void run() {
+                Test test = new Test();
+                test.printY();
+            }
+        });
+        thread2.start();
+
+        Thread thread3 = new Thread(new Runnable() {
+            public void run() {
+                Test test = new Test();
+                test.printZ();
+            }
+        });
+        thread3.start();
+
+        Thread thread4 = new Thread(new Runnable() {
+            public void run() {
+                Test test = new Test();
+                test.printD();
+            }
+        });
+        thread4.start();
+
+    }
+
+    private void printX(){
+        for(int i = 0; i < this.lenght; i++){
+            System.out.print("X");
+        }
+    }
+
+    private void printY(){
+        for(int i = 0; i < this.lenght; i++){
+            System.out.print("Y");
+        }
+    }
+
+    private void printZ(){
+        for(int i = 0; i < this.lenght; i++){
+            System.out.print("Z");
+        }
+    }
+
+    private void printD(){
+        for(int i = 0; i < this.lenght; i++){
+            System.out.print("D");
+        }
+    }
+}
--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,4 +1,4 @@
-
+import Project.Test;
 
 public class Run {
     public static void main(String[] args) {
