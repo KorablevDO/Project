f19c5783c3fb7a5901c84c26ddf39067483d0c66
do.korablev <krablevd@ya.ru>
2018-04-10 17:51:53 +0300

    Add

---
+++ TODO/src/main/java/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        /**
+         * 1. логгирование через log4j
+         * 2. сборка обекта черезе lombok
+         * 3. покрытие тестами через фреймворк TestNG
+         * 4. сборка объектов по xml через JAXBContext
+         */
+    }
+}
