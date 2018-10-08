ce42761d9268c570e3b557981b52a0cdfb0c5a78
do.korablev <krablevd@ya.ru>
2018-10-08 11:57:56 +0300

    Create generate random line

--- other/src/main/java/ru/org/tasks/TasksDataStreams.java
+++ other/src/main/java/ru/org/tasks/TasksDataStreams.java
@@ -1,5 +1,6 @@
 package ru.org.tasks;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,7 +15,31 @@ public class TasksDataStreams {
         return list;
     }
 
+    private Random random = new Random();
 
 
+    public List<String> getDataString(int size){
+        List<String> list = new ArrayList<>();
+        int max = 10;
+        for(int i = 0; i < size; i++){
+            String str = convert(getRandomStr(this.random.nextInt(max) + 1));
+            list.add(str);
+        }
+        return list;
+    }
 
+    private String getRandomStr(int size){
+        int ascMax = 79;
+        int ascMin = 41;
+        StringBuffer buffer = new StringBuffer();
+        for(int i = 0; i < size; i++){
+            String convert = Character.toString((char)(this.random.nextInt(ascMax) + ascMin));
+            buffer.append(convert);
+        }
+        return buffer.toString();
+    }
+
+    private String convert(String str){
+        return new String(str.getBytes(), StandardCharsets.UTF_8);
+    }
 }
--- other/src/test/java/ru/org/tasks/TestTasksDataStreams.java
+++ other/src/test/java/ru/org/tasks/TestTasksDataStreams.java
@@ -2,32 +2,29 @@ package ru.org.tasks;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.Assert;
-import org.testng.TestNG;
 import org.testng.annotations.Test;
-import org.testng.annotations.TestInstance;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestTasksDataStreams {
-    private static final Logger logger = LogManager.getLogger(TestTasksDataStreams.class);
-    private static final TasksDataStreams streams = new TasksDataStreams();
+    private static final Logger LOGGER = LogManager.getLogger(TestTasksDataStreams.class);
+    private static final TasksDataStreams DATA_STREAMS = new TasksDataStreams();
 
     /**
      * В методы filter и map можно передать лямда выражения и методы
      */
     @Test
     public void testConverToStream(){
-        List<Integer> list = streams.getList(100);
+        List<Integer> list = DATA_STREAMS.getList(100);
         Stream<Integer> stream = list.stream();
         List<Integer> result = stream.collect(Collectors.toList());
     }
 
     @Test
     public void testStreamFilter(){
-        List<Integer> list = streams.getList(100);
+        List<Integer> list = DATA_STREAMS.getList(100);
         /**
          * Метод filter позволяет создать новый поток с определенными данными
          */
@@ -37,12 +34,18 @@ public class TestTasksDataStreams {
 
     @Test
     public void testStreamMap(){
-        List<Integer> list = streams.getList(100);
+        List<Integer> list = DATA_STREAMS.getList(100);
         /**
          * Метод map позволяет модернизировать данные в потоке
          */
         Stream<Integer> stream = list.stream().map(integer -> integer.intValue() + 10);
         List<Integer> result = stream.collect(Collectors.toList());
+    }
+    //****************************************************************************************************
+    @Test
+    public void test1(){
+        List<String> list = DATA_STREAMS.getDataString(10);
+        LOGGER.info(list.toString());
     }
 
 }
