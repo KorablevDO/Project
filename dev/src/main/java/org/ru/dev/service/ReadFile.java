f4f387c32e904a5f9b357722cb87067a97004105
do.korablev <krablevd@ya.ru>
2018-12-12 17:45:50 +0300

    Update

--- dev/src/main/java/org/ru/dev/service/ReadFile.java
+++ dev/src/main/java/org/ru/dev/service/ReadFile.java
@@ -1,16 +1,16 @@
 package org.ru.dev.service;
 
 import java.io.*;
-import java.util.Deque;
 
 public class ReadFile {
-    private boolean limit = false;
     private char separator = ',';
 
     public void read(File file) throws IOException {
+        System.out.println(file.getName());
         InputStreamReader reader = new InputStreamReader(new FileInputStream(file));
         int data;
         StringBuffer buffer = new StringBuffer();
+        MathematicalOperation operation = new MathematicalOperation();
 
         while ((data = reader.read()) != 0){
             char c = (char) data;
@@ -18,7 +18,9 @@ public class ReadFile {
             if(c != this.separator){
                 buffer.append(c);
             } else {
-
+                String result = buffer.toString();
+                long l = operation.operationMultiplications(Integer.valueOf(result));
+//                System.out.println("File = " + file.getName() + " : old = " + result + " : new = " + l);
                 buffer.setLength(0);
             }
         }
--- dev/src/test/java/org/ru/dev/ReadFileTest.java
+++ dev/src/test/java/org/ru/dev/ReadFileTest.java
@@ -1,31 +1,14 @@
 package org.ru.dev;
 
-import org.ru.dev.service.ReadFile;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class ReadFileTest {
 
     public void readFileInteger() throws IOException {
-        String path = "dev/src/main/resources/dataPull0.txt";
-        File file = new File(path);
-        ReadFile<Integer> reader = new ReadFile<Integer>();
-        Deque<Integer> deque = new ArrayDeque<>();
-        reader.setDeque(deque);
-        reader.read(file);
-        System.out.println(deque.toString());
+
     }
 
     public void readFileString() throws IOException {
-        String path = "dev/src/main/resources/dataPull0.txt";
-        File file = new File(path);
-        ReadFile<String> reader = new ReadFile<String>();
-        Deque<String> deque = new ArrayDeque<>();
-        reader.setDeque(deque);
-        reader.read(file);
-        System.out.println(deque.toString());
+
     }
 }
