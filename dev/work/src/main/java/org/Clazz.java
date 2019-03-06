61640b3ba91af8bc319a5bac1088a43d128f2188
KorablevDO <krablevd@ya.ru>
2019-03-06 08:25:20 +0300

    Create work.

---
+++ dev/work/src/main/java/org/Clazz.java
@@ -0,0 +1,10 @@
+package org;
+
+/**
+ * Created by 1 on 05.03.2019.
+ */
+public class Clazz {
+//https://www.ibm.com/developerworks/ru/library/j-5things13/index.html
+    ///https://www.youtube.com/watch?v=LJjIaCKuzoc
+    //https://www.javatpoint.com/html-button-tag
+}
